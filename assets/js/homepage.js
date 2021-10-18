51b30107152a4d8a5c4399532ddfe683035663fe
Nathan Rose <fullstackroses@gmail.com>
2021-10-18 09:04:33 -0500

    feature/user-repo

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormE1 = document.querySelector("#user-form");
+var nameInputE1 = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // format the github api url 
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -9,3 +12,22 @@ var getUserRepos = function(user) {
          });
      });
 };
+
+
+var formSubmitHandler = function(event) {
+      event.preventDefault();
+      //get value from input
+      var username = nameInputE1.value.trim();
+
+      if (username) {
+          getUserRepos(username);
+          nameInputE1.value = "";
+      } else {
+          alert("PLEASE enter a Github username");
+      }
+
+        console.log(event);
+};
+
+
+userFormE1.addEventListener("submit", formSubmitHandler);
